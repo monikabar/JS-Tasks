394b3891d43735d894a0606ef8665bf990518fe2
Monika Barkauskaite <you@example.com>
2022-08-31 10:42:33 +0300

    New Features

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,19 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+    constructor(title, director, budget){
+        this.title = title;
+        this.director = director;
+        this. budget = budget;
+    }
+
+    wasExpensive(){
+        return this.budget > 100000000? true: false
+    }
+}
+
+    const myMovie1 = new Movie('Where the crawdads sing', 'Olivia Newman', '24000000');
+    const myMovie2 = new Movie('Avatar', 'James Cameron', '237000000');
+
+    console.log(myMovie1.wasExpensive(), myMovie2.wasExpensive());
