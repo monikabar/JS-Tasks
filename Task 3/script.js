e8dca47f320fb85c56d52e3943c796c11b79020c
Monika Barkauskaite <you@example.com>
2022-09-02 19:11:18 +0300

    New Features

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,42 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+const message = document.getElementById("message");
+
+btn.addEventListener("click", showGithubUsers);
+
+function showGithubUsers() {
+  fetch(ENDPOINT)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      data.forEach((el) => {
+        let card = document.createElement("div");
+        card.textContent = `${el.login}`;
+
+        card.style.border = "black solid";
+        card.style.marginBlock = "1rem";
+        card.style.backgroundColor = "lightgrey";
+        card.style.textAlign = "center";
+        card.style.fontSize = "16pt";
+        card.style.fontWeight = "600";
+
+        let avatar = document.createElement("img");
+        card.appendChild(avatar);
+
+        avatar.setAttribute("src", el.avatar_url);
+        avatar.style.display = "block";
+        avatar.style.height = "100px";
+        avatar.style.width = "100px";
+
+        output.appendChild(card);
+
+        message.style.display = "none";
+      });
+    });
+}
